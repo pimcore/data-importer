ad59668a7f46dfcbb3d51aeaa4da7e76486793b8
sissonen <71624042+sissonen@users.noreply.github.com>
2021-07-26 10:13:30 +0300

    Create ConditionalConversion.php

---
+++ src/Mapping/Operator/Simple/ConditionalConversion.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\DataImporterBundle\Mapping\Operator\Simple;
+
+use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Type\TransformationDataTypeService;
+
+class ConditionalConversion extends AbstractOperator
+{
+
+    /**
+     * @var string
+     */
+    protected $original;
+
+    /**
+     * @var string
+     */
+    protected $converted;
+
+    public function setSettings(array $settings): void
+    {
+        $this->original = $settings['original'] ?? '';
+        $this->converted = $settings['converted'] ?? '';
+    }
+
+    public function process($inputData, bool $dryRun = false)
+    {
+        $returnScalar = false;
+        if (!is_array($inputData)) {
+            $returnScalar = true;
+            $inputData = [$inputData];
+        }
+
+        \Pimcore\Log\Simple::log("importerlog", $this->original . " " . $this->converted ." ".print_r($inputData, true));
+        $origArr = explode('|', $this->original);
+        $convArr = explode('|', $this->converted);
+        foreach ($inputData as &$data) {
+            $index = array_search($data, $origArr);
+            if ($index !== false) {
+                $data = $convArr[$index];
+            } else {
+                $index = array_search('*', $origArr);
+                if ($index !== false) {
+                    $data = $convArr[$index];
+                }
+            }
+        }
+
+        if ($returnScalar) {
+            if (!empty($inputData)) {
+                return reset($inputData);
+            }
+
+            return null;
+        } else {
+            return $inputData;
+        }
+    }
+    
+    public function evaluateReturnType(string $inputType, int $index = null): string
+    {
+        if (!in_array($inputType, [TransformationDataTypeService::DEFAULT_TYPE, TransformationDataTypeService::DEFAULT_ARRAY])) {
+            throw new InvalidConfigurationException(sprintf("Unsupported input type '%s' for simple test operator at transformation position %s", $inputType, $index));
+        }
+
+        return $inputType;
+    }
+}
