3c86b9ec0fd6b0ed068db4c37abee6687b726461
sissonen <71624042+sissonen@users.noreply.github.com>
2021-08-06 15:12:07 +0300

    Update loadDataObject.js

--- src/Resources/public/js/pimcore/configuration/components/mapping/operator/loadDataObject.js
+++ src/Resources/public/js/pimcore/configuration/components/mapping/operator/loadDataObject.js
@@ -93,7 +93,7 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.mapping.operat
         });
         
         const partialMatch = Ext.create('Ext.form.field.Checkbox', {
-            fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_accept_partial_match',
+            fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_transformation_pipeline_accept_partial_match',
             name: 'settings.partialMatch',
             allowBlank: true,
             value: this.data.settings ? this.data.settings.partialMatch : false,
