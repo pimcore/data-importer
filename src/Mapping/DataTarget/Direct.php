31d14881eed38c79aba9504a1708d66387cd4131
woellundwoell <woellundwoell@users.noreply.github.com>
2021-09-28 06:19:18 +0000

    Apply php-cs-fixer changes

--- src/Mapping/DataTarget/Direct.php
+++ src/Mapping/DataTarget/Direct.php
@@ -9,8 +9,8 @@
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Bundle\DataImporterBundle\Mapping\DataTarget;
@@ -56,26 +56,22 @@ class Direct implements DataTargetInterface
      */
     public function setSettings(array $settings): void
     {
-        if (empty($settings['fieldName']))
-        {
+        if (empty($settings['fieldName'])) {
             throw new InvalidConfigurationException('Empty field name.');
         }
 
         $this->fieldName = $settings['fieldName'];
         $this->language = $settings['language'] ?? null;
 
-        if (isset($settings['writeIfSourceIsEmpty']))
-        {
+        if (isset($settings['writeIfSourceIsEmpty'])) {
             $this->writeIfSourceIsEmpty = $settings['writeIfSourceIsEmpty'];
         }
 
-        if (isset($settings['writeIfTargetIsNotEmpty']))
-        {
+        if (isset($settings['writeIfTargetIsNotEmpty'])) {
             $this->writeIfTargetIsNotEmpty = $settings['writeIfTargetIsNotEmpty'];
         }
 
-        if (isset($settings['appendRelationItems']))
-        {
+        if (isset($settings['appendRelationItems'])) {
             $this->appendRelationItems = $settings['appendRelationItems'];
         }
     }
@@ -92,18 +88,15 @@ class Direct implements DataTargetInterface
     {
         $setterParts = explode('.', $this->fieldName);
 
-        if (count($setterParts) === 1)
-        {
+        if (count($setterParts) === 1) {
             //direct class attribute
             $setter = 'set' . ucfirst($this->fieldName);
             $getter = 'get' . ucfirst($this->fieldName);
-            if (!$this->checkAssignData($data, $element->$getter($this->language)))
-            {
+            if (!$this->checkAssignData($data, $element->$getter($this->language))) {
                 return;
             }
             $element->$setter($this->getPreprocessData($element, $element->getClass(), $this->fieldName, $data), $this->language);
-        } elseif (count($setterParts) === 3)
-        {
+        } elseif (count($setterParts) === 3) {
             //brick attribute
 
             $brickContainerGetter = 'get' . ucfirst($setterParts[0]);
@@ -112,8 +105,7 @@ class Direct implements DataTargetInterface
             $brickGetter = 'get' . ucfirst($setterParts[1]);
             $brick = $brickContainer->$brickGetter();
 
-            if (empty($brick))
-            {
+            if (empty($brick)) {
                 $brickClassName = '\\Pimcore\\Model\\DataObject\\Objectbrick\\Data\\' . ucfirst($setterParts[1]);
                 $brick = new $brickClassName($element);
                 $brickSetter = 'set' . ucfirst($setterParts[1]);
@@ -122,13 +114,11 @@ class Direct implements DataTargetInterface
 
             $setter = 'set' . ucfirst($setterParts[2]);
             $getter = 'get' . ucfirst($setterParts[2]);
-            if (!$this->checkAssignData($data, $brick->$getter($this->language)))
-            {
+            if (!$this->checkAssignData($data, $brick->$getter($this->language))) {
                 return;
             }
             $brick->$setter($this->getPreprocessData($brick, $brick->getDefinition(), $setterParts[2], $data), $this->language);
-        } else
-        {
+        } else {
             throw new InvalidConfigurationException('Invalid number of setter parts for ' . $this->fieldName);
         }
     }
@@ -145,14 +135,14 @@ class Direct implements DataTargetInterface
     {
         $fieldDef = $definition->getFieldDefinition($attributeName);
 
-        switch ($fieldDef->getFieldtype())
-        {
+        switch ($fieldDef->getFieldtype()) {
             case 'manyToManyRelation':
             case 'manyToManyObjectRelation':
             case 'advancedManyToManyRelation':
             case 'advancedManyToManyObjectRelation':
                 $getter = 'get' . ucfirst($attributeName);
                 $existingData = $object->$getter();
+
                 return $this->getMergedDataArray($existingData ?? [], $data, $fieldDef->getFieldtype());
 
             default:
@@ -164,47 +154,39 @@ class Direct implements DataTargetInterface
      * @param array $existingData
      * @param array $data
      * @param string $fieldType
+     *
      * @return array
+     *
      * @throws \Exception
      */
     protected function getMergedDataArray(array $existingData, array $data, string $fieldType): array
     {
         $newData = [];
-        switch ($fieldType)
-        {
+        switch ($fieldType) {
             case 'manyToManyObjectRelation':
-                if ($this->appendRelationItems)
-                {
-                    foreach ($existingData as $dataObject)
-                    {
+                if ($this->appendRelationItems) {
+                    foreach ($existingData as $dataObject) {
                         $newData[$dataObject->getId()] = $dataObject;
                     }
 
-                    foreach ($data as $dataObject)
-                    {
-                        if (!isset($newData[$dataObject->getId()]))
-                        {
+                    foreach ($data as $dataObject) {
+                        if (!isset($newData[$dataObject->getId()])) {
                             $newData[$dataObject->getId()] = $dataObject;
                         }
                     }
-                } else
-                {
+                } else {
                     return $data;
                 }
                 break;
 
             case 'advancedManyToManyObjectRelation':
-                if ($this->appendRelationItems)
-                {
-                    foreach ($existingData as $metaDataObject)
-                    {
+                if ($this->appendRelationItems) {
+                    foreach ($existingData as $metaDataObject) {
                         $newData[$metaDataObject->getObject()->getId()] = $metaDataObject;
                     }
                 }
-                foreach ($data as $dataObject)
-                {
-                    if (!$this->appendRelationItems || !isset($newData[$dataObject->getId()]))
-                    {
+                foreach ($data as $dataObject) {
+                    if (!$this->appendRelationItems || !isset($newData[$dataObject->getId()])) {
                         $metaDataObject = new ObjectMetadata($this->fieldName, [], $dataObject);
                         $newData[$metaDataObject->getObject()->getId()] = $metaDataObject;
                     }
@@ -213,40 +195,31 @@ class Direct implements DataTargetInterface
                 break;
 
             case 'manyToManyRelation':
-                if ($this->appendRelationItems)
-                {
-                    foreach ($existingData as $element)
-                    {
+                if ($this->appendRelationItems) {
+                    foreach ($existingData as $element) {
                         $newData[Service::getElementType($element) . '_' . $element->getId()] = $element;
                     }
-                    foreach ($data as $element)
-                    {
-                        if (!isset($newData[Service::getElementType($element) . '_' . $element->getId()]))
-                        {
+                    foreach ($data as $element) {
+                        if (!isset($newData[Service::getElementType($element) . '_' . $element->getId()])) {
                             $newData[Service::getElementType($element) . '_' . $element->getId()] = $element;
                         }
                     }
-                } else
-                {
+                } else {
                     return $data;
                 }
 
                 break;
 
             case 'advancedManyToManyRelation':
-                if ($this->appendRelationItems)
-                {
-                    foreach ($existingData as $metaDataElement)
-                    {
+                if ($this->appendRelationItems) {
+                    foreach ($existingData as $metaDataElement) {
                         $newData[Service::getElementType($metaDataElement->getElement()) . '_' .
                         $metaDataElement->getElement()->getId()] = $metaDataElement;
                     }
                 }
-                foreach ($data as $element)
-                {
+                foreach ($data as $element) {
                     if (!$this->appendRelationItems ||
-                        !isset($newData[Service::getElementType($metaDataElement->getElement()) . '_' . $element->getId()]))
-                    {
+                        !isset($newData[Service::getElementType($metaDataElement->getElement()) . '_' . $element->getId()])) {
                         $metaDataElement = new ElementMetadata($this->fieldName, [], $element);
                         $newData[Service::getElementType($metaDataElement->getElement()) . '_' . $element->getId()] =
                             $metaDataElement;
@@ -266,13 +239,11 @@ class Direct implements DataTargetInterface
      */
     protected function checkAssignData($valueData, $valueAttribute)
     {
-        if (!empty($valueAttribute) && $this->writeIfTargetIsNotEmpty === false)
-        {
+        if (!empty($valueAttribute) && $this->writeIfTargetIsNotEmpty === false) {
             return false;
         }
         if ((empty($valueData) || ($valueData instanceof QuantityValue && empty($valueData->getValue()))) &&
-            $this->writeIfSourceIsEmpty === false)
-        {
+            $this->writeIfSourceIsEmpty === false) {
             return false;
         }
 
