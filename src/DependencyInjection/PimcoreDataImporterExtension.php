698c996bb2279a63dad56952c903aa8586fd1983
dvesh3 <dvesh3@users.noreply.github.com>
2021-11-12 13:47:05 +0000

    Apply php-cs-fixer changes

--- src/DependencyInjection/PimcoreDataImporterExtension.php
+++ src/DependencyInjection/PimcoreDataImporterExtension.php
@@ -18,8 +18,8 @@ namespace Pimcore\Bundle\DataImporterBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
