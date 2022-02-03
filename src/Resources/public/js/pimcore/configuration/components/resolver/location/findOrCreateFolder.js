95e900dde0fb05feb8ee3c1e6d06bb4b8a8eed09
Jia Jia Ji <kingjia90@gmail.com>
2022-02-03 12:44:23 +0100

    clean up of js, ready to PR

--- src/Resources/public/js/pimcore/configuration/components/resolver/location/findOrCreateFolder.js
+++ src/Resources/public/js/pimcore/configuration/components/resolver/location/findOrCreateFolder.js
@@ -15,90 +15,10 @@ pimcore.registerNS('pimcore.plugin.pimcoreDataImporterBundle.configuration.compo
 pimcore.plugin.pimcoreDataImporterBundle.configuration.components.resolver.location.findOrCreateFolder = Class.create(pimcore.plugin.pimcoreDataImporterBundle.configuration.components.abstractOptionType, {
 
     type: 'findOrCreateFolder',
-    dataApplied: false,
 
     buildSettingsForm: function() {
 
         if(!this.form) {
-
-            const languageSelection = Ext.create('Ext.form.ComboBox', {
-                store: pimcore.settings.websiteLanguages,
-                forceSelection: true,
-                fieldLabel: t('language'),
-                name: this.dataNamePrefix + 'attributeLanguage',
-                value: this.data.attributeLanguage,
-                allowBlank: true,
-                hidden: true
-            });
-
-            const attributeName = Ext.create('Ext.form.ComboBox', {
-                fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_attribute_name'),
-                name: this.dataNamePrefix + 'attributeName',
-                hidden: this.data.findStrategy !== 'attribute',
-                allowBlank: true, // this.data.findStrategy !== 'attribute',
-                value: this.data.attributeName,
-                displayField: 'title',
-                valueField: 'key',
-                forceSelection: true,
-                queryMode: 'local'
-            });
-
-
-            const attributeStore = Ext.create('Ext.data.JsonStore', {
-                fields: ['key', 'name', 'localized'],
-                autoLoad: true,
-                proxy: {
-                    type: 'ajax',
-                    extraParams: {
-                        class_id: this.data.attributeDataObjectClassId,
-                        system_read: 1
-                    },
-                    url: Routing.generate('pimcore_dataimporter_configdataobject_loaddataobjectattributes'),
-                    reader: {
-                        type: 'json',
-                        rootProperty: 'attributes'
-                    }
-                },
-
-                listeners: {
-                    dataChanged: function(store) {
-                        if(!this.dataApplied) {
-                            attributeName.setValue(this.data.attributeName);
-                            this.form.isValid();
-                            this.dataApplied = true;
-                            this.setLanguageVisibility(attributeStore, attributeName, languageSelection);
-                        }
-                    }.bind(this)
-                }
-            });
-
-            attributeName.setStore(attributeStore);
-            attributeName.on('change', this.setLanguageVisibility.bind(this, attributeStore, attributeName, languageSelection));
-
-
-            const attributeDataObjectClassId = Ext.create('Ext.form.field.ComboBox', {
-                typeAhead: true,
-                triggerAction: 'all',
-                store: pimcore.globalmanager.get('object_types_store'),
-                valueField: 'id',
-                displayField: 'text',
-                listWidth: 'auto',
-                fieldLabel: t('class'),
-                name: this.dataNamePrefix + 'attributeDataObjectClassId',
-                value:  this.data.attributeDataObjectClassId,
-                hidden: this.data.findStrategy !== 'attribute',
-                allowBlank: true, // this.data.findStrategy !== 'attribute',
-                forceSelection: true,
-                listeners: {
-                    change: function(combo, newValue, oldValue) {
-                        attributeStore.proxy.setExtraParam('class_id', newValue);
-                        attributeStore.load();
-                    }
-                }
-            });
-
-
-
 
             this.form = Ext.create('DataHub.DataImporter.StructuredValueForm', {
                 defaults: {
@@ -109,9 +29,6 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.resolver.locat
                 },
                 border: false,
                 items: [
-                    attributeDataObjectClassId,
-                    attributeName,
-                    languageSelection,
                     {
                         xtype: 'combo',
                         fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_data_source_index'),
@@ -134,17 +51,6 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.resolver.locat
         }
 
         return this.form;
-    },
-
-
-    setLanguageVisibility: function(attributeStore, attributeSelection, languageSelection) {
-        const record = attributeStore.findRecord('key', attributeSelection.getValue());
-        if(record) {
-            languageSelection.setHidden(!record.data.localized);
-            if(!record.data.localized) {
-                languageSelection.setValue(null);
-            }
-        }
     }
 
 });
