c86d5599057dfb64f31631710d1cade26f989fe9
mcop1 <marco.perberschlager@pimcore.com>
2022-04-28 15:03:06 +0200

    [Feature] Allow assigning values to geo data types

---
+++ src/Mapping/Operator/Factory/AsGeobounds.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\DataImporterBundle\Mapping\Operator\Factory;
+
+use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Type\TransformationDataTypeService;
+use Pimcore\Model\DataObject\Data\GeoCoordinates;
+use Pimcore\Model\DataObject\Data\Geobounds;
+
+class AsGeobounds extends AbstractOperator
+{
+    public function process($inputData, bool $dryRun = false)
+    {
+        $northEast = new GeoCoordinates($inputData[0] ?? null, $inputData[1] ?? null);
+        $southWest = new GeoCoordinates($inputData[2] ?? null, $inputData[3] ?? null);
+        return new Geobounds($northEast, $southWest);
+    }
+
+    public function generateResultPreview($inputData)
+    {
+        if($inputData instanceof Geobounds) {
+            return "NE: " . $inputData->getNorthEast() . " SW: " . $inputData->getSouthWest();
+        }
+        return $inputData;
+    }
+
+    /**
+     * @param string $inputType
+     * @param int|null $index
+     *
+     * @return string
+     *
+     * @throws InvalidConfigurationException
+     */
+    public function evaluateReturnType(string $inputType, int $index = null): string
+    {
+        if ($inputType !== TransformationDataTypeService::DEFAULT_ARRAY) {
+            throw new InvalidConfigurationException(sprintf("Unsupported input type '%s' for geoBounds operator at transformation position %s", $inputType, $index));
+        }
+
+        return TransformationDataTypeService::GEOBOUNDS_VALUE;
+    }
+}
---
+++ src/Mapping/Operator/Factory/AsGeopoint.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\DataImporterBundle\Mapping\Operator\Factory;
+
+use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Type\TransformationDataTypeService;
+use Pimcore\Model\DataObject\Data\GeoCoordinates;
+
+class AsGeopoint extends AbstractOperator
+{
+    public function process($inputData, bool $dryRun = false)
+    {
+        return new GeoCoordinates($inputData[0] ?? null, $inputData[1] ?? null);
+    }
+
+    public function generateResultPreview($inputData)
+    {
+        if($inputData instanceof GeoCoordinates) {
+            return "Lat.: " . $inputData->getLongitude() . "  Long.: " . $inputData->getLatitude();
+        }
+        return $inputData;
+    }
+
+    /**
+     * @param string $inputType
+     * @param int|null $index
+     *
+     * @return string
+     *
+     * @throws InvalidConfigurationException
+     */
+    public function evaluateReturnType(string $inputType, int $index = null): string
+    {
+        if ($inputType !== TransformationDataTypeService::DEFAULT_ARRAY) {
+            throw new InvalidConfigurationException(sprintf("Unsupported input type '%s' for geoPoint operator at transformation position %s", $inputType, $index));
+        }
+
+        return TransformationDataTypeService::GEOPOINT_VALUE;
+    }
+}
