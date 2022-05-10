9c0c6f3b3a14e0ac50d06709509dbb90369c667f
Andrew Zhdanovskikh <andrew72ru@gmail.com>
2022-05-10 12:02:25 +0200

    Fix 0 index for attribute-based strategy
    Closes #202

---
+++ tests/unit/AttributeBasedStrategyTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Pimcore\Bundle\DataImporterBundle\Tests\unit;
+
+use Pimcore\Bundle\DataImporterBundle\Resolver\Publish\AttributeBasedStrategy;
+
+class AttributeBasedStrategyTest extends \Codeception\Test\Unit
+{
+    protected $tester;
+
+    public function provideIndexes(): array
+    {
+        return [
+            ["0"],
+            [0],
+            [1],
+            ["12"],
+        ];
+    }
+
+    public function testDataSourceIndex(mixed $index): void
+    {
+        $config = ['dataSourceIndex' => $index];
+        $strategy = new AttributeBasedStrategy();
+        $dataSourceIndex = (new \ReflectionObject($strategy))->getProperty('dataSourceIndex');
+        $dataSourceIndex->setAccessible(true);
+
+        $strategy->setSettings($config);
+        $result = $dataSourceIndex->getValue($strategy);
+        self::assertEquals($index, $result);
+    }
+}
