89bb5505e08756190965affbca5fa4fab6185a3e
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2022-05-24 10:47:07 +0200

    str_replace operator in mapping transformation pipeline (#209)
    * add str_replace operator to transformation pipeline
    
    * add tests for str_replace operator in transformation pipeline
    
    * Apply php-cs-fixer changes
    
    Co-authored-by: robertSt7 <robertSt7@users.noreply.github.com>

---
+++ src/Mapping/Operator/Simple/StringReplace.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\DataImporterBundle\Mapping\Operator\Simple;
+
+use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
+use Pimcore\Bundle\DataImporterBundle\Mapping\Type\TransformationDataTypeService;
+
+class StringReplace extends AbstractOperator
+{
+    protected string $search;
+    protected string $replace;
+
+    public function setSettings(array $settings): void
+    {
+        $this->search = $settings['search'] ?? '';
+        $this->replace = $settings['replace'] ?? '';
+    }
+
+    public function process($inputData, bool $dryRun = false)
+    {
+        $returnScalar = false;
+        if (!is_array($inputData)) {
+            $returnScalar = true;
+            $inputData = [$inputData];
+        }
+
+        foreach ($inputData as &$data) {
+            $data = str_replace($this->search, $this->replace, $data);
+        }
+
+        if ($returnScalar) {
+            if (!empty($inputData)) {
+                return reset($inputData);
+            }
+
+            return null;
+        } else {
+            return $inputData;
+        }
+    }
+
+    /**
+     * @param string $inputType
+     * @param int|null $index
+     *
+     * @return string
+     *
+     * @throws InvalidConfigurationException
+     */
+    public function evaluateReturnType(string $inputType, int $index = null): string
+    {
+        if (!in_array($inputType, [TransformationDataTypeService::DEFAULT_TYPE, TransformationDataTypeService::DEFAULT_ARRAY])) {
+            throw new InvalidConfigurationException(sprintf("Unsupported input type '%s' for string replace operator at transformation position %s", $inputType, $index));
+        }
+
+        return $inputType;
+    }
+}
