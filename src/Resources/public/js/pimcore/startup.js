d292380658acd55d6623af296b3d1f4ddec01b30
robertSt7 <robert.steinkellner@pimcore.com>
2022-09-15 08:34:21 +0200

    Improve: Remove plugins

--- src/Resources/public/js/pimcore/startup.js
+++ src/Resources/public/js/pimcore/startup.js
@@ -11,17 +11,4 @@
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-pimcore.registerNS("pimcore.plugin.PimcoreDataImporterBundle");
 
-pimcore.plugin.PimcoreDataImporterBundle = Class.create(pimcore.plugin.admin, {
-    getClassName: function () {
-        return 'pimcore.plugin.PimcoreDataImporterBundle';
-    },
-
-    initialize: function () {
-        pimcore.plugin.broker.registerPlugin(this);
-    }
-
-});
-
-var PimcoreDataImporterBundlePlugin = new pimcore.plugin.PimcoreDataImporterBundle();
