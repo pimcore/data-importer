30803063441b2292434a29e8f63d0927c41a4bd1
Aashan Ghimire <ashanghimire10@gmail.com>
2022-10-19 19:13:49 +0545

    [Actions]: Replace extensions.php in favor of bundles.php

--- .github/ci/files/extensions.template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "bundle" => [
-        "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" => FALSE,
-        "Pimcore\\Bundle\\DataImporterBundle\\PimcoreDataImporterBundle" => TRUE
-    ]
-];
