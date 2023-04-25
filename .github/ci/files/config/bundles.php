3adaaa012b4164857e62a61d4e702ed3071965b2
Marco Perberschlager <marco.perberschlager@pimcore.com>
2023-04-25 13:05:01 +0200

    Adapted github workflows

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,12 @@
 <?php
 
+if(\Pimcore\Version::getMajorVersion() >= 11) {
+    return [
+        \Pimcore\Bundle\AdminBundle\PimcoreAdminBundle::class => ['all' => true],
+        \Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle::class => ['all' => true]
+    ];
+}
+
 return [
-    'Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle' => ['all' => false],
-    'Pimcore\\Bundle\\DataImporterBundle\\PimcoreDataImporterBundle' => ['all' => true]
+    \Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle::class => ['all' => true]
 ];
