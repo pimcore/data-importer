79ae6c50f9a28082bf3d6d7396cbd000a333657f
JiaJia Ji <kingjia90@gmail.com>
2025-03-17 17:25:35 +0100

    [Improvement]: Add support to DBAL v4 (#462)

--- src/DataSource/Loader/SqlLoader.php
+++ src/DataSource/Loader/SqlLoader.php
@@ -63,7 +63,7 @@ class SqlLoader implements DataLoaderInterface
             $queryBuilder->groupBy($this->groupBy);
         }
 
-        $result = $queryBuilder->execute()->fetchAllAssociative();
+        $result = $queryBuilder->executeQuery()->fetchAllAssociative();
 
         $filesystemLocal = new Filesystem(new LocalFilesystemAdapter('/'));
         $stream = fopen('php://temp', 'r+');
