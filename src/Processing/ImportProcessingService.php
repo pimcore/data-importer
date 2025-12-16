851622a4c51deff353af8972cba89ec3bb96de61
JiaJia Ji <kingjia90@gmail.com>
2025-12-16 09:33:31 +0100

    Update ImportProcessingService.php

--- src/Processing/ImportProcessingService.php
+++ src/Processing/ImportProcessingService.php
@@ -295,7 +295,7 @@ class ImportProcessingService
                 );
                 $this->eventDispatcher->dispatch($event);
             } else {
-                $message .= "\nProcessElementExceptionEvent skipped as element is not found."
+                $message .= "\nProcessElementExceptionEvent skipped as element is not found.";
             }
 
             $this->logError($configName, $message, [
