bd50e770eb1c5fe5043ad229d857ab0959ca36ac
Christian Fasching <christian.fasching@pimcore.com>
2026-01-23 09:53:39 +0100

    added missing interface

---
+++ src/Settings/DataTargetFieldValidatorInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\DataImporterBundle\Settings;
+
+use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+
+/**
+ * Interface for data targets that support field validation against
+ * transformation result types
+ */
+interface DataTargetFieldValidatorInterface
+{
+    /**
+     * Validate that the configured target field is compatible with the
+     * transformation result type
+     *
+     * @throws InvalidConfigurationException When field is incompatible
+     *     with transformation result type
+     */
+    public function validateTargetField(
+        string $transformationResultType,
+        string $classId
+    ): void;
+}
