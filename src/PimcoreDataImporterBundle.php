4911f1f822279f11d08371457c12d66748fe572b
jcPimcore <jochen.califice@pimcore.com>
2026-03-04 15:11:43 +0100

    Improve: Removing ExtJS

--- src/PimcoreDataImporterBundle.php
+++ src/PimcoreDataImporterBundle.php
@@ -54,9 +54,7 @@ class PimcoreDataImporterBundle extends AbstractPimcoreBundle implements Depende
      */
     public function getCssPaths(): array
     {
-        return [
-            '/bundles/pimcoredataimporter/css/icons.css'
-        ];
+        return [];
     }
 
     /**
@@ -64,81 +62,7 @@ class PimcoreDataImporterBundle extends AbstractPimcoreBundle implements Depende
      */
     public function getJsPaths(): array
     {
-        return [
-            '/bundles/pimcoredataimporter/js/pimcore/helper/ext_extensions.js',
-            '/bundles/pimcoredataimporter/js/pimcore/helper/abstractOptionType.js',
-            '/bundles/pimcoredataimporter/js/pimcore/adapter/dataImporterDataObject.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/configEvents.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/configItemDataObject.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/sftp.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/http.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/asset.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/upload.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/push.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/loader/sql.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/interpreter/csv.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/interpreter/json.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/interpreter/xlsx.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/interpreter/xml.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/interpreter/sql.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/cleanup/unpublish.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/cleanup/delete.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/importSettings.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/importPreview.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/load/id.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/load/path.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/load/attribute.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/load/notLoad.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/location/staticPath.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/location/findParent.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/location/findOrCreateFolder.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/location/noChange.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/location/doNotCreate.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/publish/alwaysPublish.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/publish/attributeBased.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/publish/noChangePublishNew.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/resolver/publish/noChangeUnpublishNew.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/mappingConfiguration.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/mappingConfigurationItem.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/transformationResultHandler.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/datatarget/direct.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/datatarget/manyToManyRelation.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/datatarget/classificationstore.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/datatarget/classificationstoreBatch.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/tools/classificationStoreKeySearchWindow.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/abstractOperator.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/trim.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/numeric.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asArray.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asCountries.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asGeopoint.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asGeobounds.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asGeopolygon.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asGeopolyline.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/asColor.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/explode.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/combine.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/htmlDecode.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/quantityValue.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/quantityValueArray.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/inputQuantityValue.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/inputQuantityValueArray.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/boolean.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/date.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/importAsset.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/loadAsset.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/gallery.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/imageAdvanced.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/loadDataObject.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/objectField.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/reduceArrayKeyValuePairs.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/flattenArray.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/staticText.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/conditionalConversion.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/mapping/operator/stringReplace.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/execution.js',
-            '/bundles/pimcoredataimporter/js/pimcore/configuration/components/logTab.js',
-        ];
+        return [];
     }
 
     public function build(ContainerBuilder $container): void
