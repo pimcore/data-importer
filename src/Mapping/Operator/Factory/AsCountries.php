0cce5ecef429262ed476ee43550007f98ece5279
robertSt7 <robert.steinkellner@pimcore.com>
2026-03-05 07:21:52 +0000

    Fix: change namespace for AppliocationLogger

--- src/Mapping/Operator/Factory/AsCountries.php
+++ src/Mapping/Operator/Factory/AsCountries.php
@@ -16,7 +16,7 @@ use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
 use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
 use Pimcore\Bundle\DataImporterBundle\Mapping\Type\TransformationDataTypeService;
 use Pimcore\Localization\LocaleServiceInterface;
-use Pimcore\Log\ApplicationLogger;
+use Pimcore\Bundle\ApplicationLoggerBundle\ApplicationLogger;
 
 class AsCountries extends AbstractOperator
 {
