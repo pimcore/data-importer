370b825eef4530450f2555de0f14e9dd2b0ee2d6
Tine <42359615+xIrusux@users.noreply.github.com>
2026-03-11 12:45:18 +0100

    [Studio] Basic studio UI setup (#532)

---
+++ src/Resources/public/studio/build/194bbd73-61f9-48d4-ac3a-7635e412534e/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/194bbd73-61f9-48d4-ac3a-7635e412534e/static/js/remoteEntry.js"
+    
