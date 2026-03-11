9dc83f5c12b964fd9659eeafc9b4a66c51c07c5d
lukemacausland <58705994+lukemacausland@users.noreply.github.com>
2026-03-11 12:22:57 +0000

    Automatic frontend build

---
+++ src/Resources/public/studio/build/1c5d222c-0a0a-4016-ba45-b10512cbc05d/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/1c5d222c-0a0a-4016-ba45-b10512cbc05d/static/js/remoteEntry.js"
+    
