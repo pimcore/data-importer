6f7043864ffeb8331ba2643ddfe0685935bb60fa
JiaJia Ji <kingjia90@gmail.com>
2026-03-24 17:15:56 +0100

    [Task]: Bump phpspreadsheet version requirement (#534)
    * Update phpspreadsheet version requirement
    
    * Remove pimcore/admin-ui-classic-bundle dependency
    
    * Change pimcore/data-hub version to dev-remove-admin-classic
    
    * Automatic frontend build
    
    * Update composer.json
    
    ---------
    
    Co-authored-by: kingjia90 <6014195+kingjia90@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/76f9c6a8-e2ae-4707-8eae-469be12b0690/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/76f9c6a8-e2ae-4707-8eae-469be12b0690/static/js/remoteEntry.js"
+    
