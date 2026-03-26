ae2068038731ab5fb531ac80aad6cfcbf2439aec
JochenC <jochen.califice@pimcore.com>
2026-03-26 07:53:22 +0100

    Remove doctrine enum mapping (#569)
    * [TASK] Remove enum mapping
    
    * [Task] Update doctrine/dbal
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: jcPimcore <259032526+jcPimcore@users.noreply.github.com>

--- src/Resources/public/studio/build/09caf80d-2474-4eaf-99b9-64c8e92cb2a3/exposeRemote.js
+++ src/Resources/public/studio/build/09caf80d-2474-4eaf-99b9-64c8e92cb2a3/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/633d98dd-b44b-45a1-97f0-95d23a80a04f/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/09caf80d-2474-4eaf-99b9-64c8e92cb2a3/static/js/remoteEntry.js"
 
       
     
