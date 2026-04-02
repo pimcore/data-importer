cbd540b7f218aa3dea6729a4bf438655cebcf33f
markus-moser <markus.moser@pimcore.com>
2026-04-02 19:48:29 +0200

    [Studio] Normalize empty PHP arrays in backend config to prevent form reset (#584)

---
+++ src/Resources/public/studio/build/50730ca3-820c-47f9-97b3-02eebf781187/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/50730ca3-820c-47f9-97b3-02eebf781187/static/js/remoteEntry.js"
+
+      
+    
