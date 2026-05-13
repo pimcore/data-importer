7afde954ea80a77e6ad07f3b24af26bd43dce655
markus-moser <markus.moser@pimcore.com>
2026-05-13 09:39:38 +0200

    [Studio] Sort import logs by id desc to match application logger (#611)

--- src/Resources/public/studio/build/a1967b31-bce1-40d4-9682-8745baf486af/exposeRemote.js
+++ src/Resources/public/studio/build/a1967b31-bce1-40d4-9682-8745baf486af/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/003114d1-1b75-4f4a-b35c-07e906ea94ab/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/a1967b31-bce1-40d4-9682-8745baf486af/static/js/remoteEntry.js"
 
       
     
