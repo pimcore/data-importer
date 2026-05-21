1c650982ca1166a27a051766fde6ca55138d6715
markus-moser <markus.moser@pimcore.com>
2026-05-21 15:06:32 +0000

    Show spinner in preview result column while loading next row

--- src/Resources/public/studio/build/1448a692-dc0d-480f-9fc0-488f61b5618f/exposeRemote.js
+++ src/Resources/public/studio/build/1448a692-dc0d-480f-9fc0-488f61b5618f/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/e852ef3c-86c3-410c-9f0b-fdcb46d4e153/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/1448a692-dc0d-480f-9fc0-488f61b5618f/static/js/remoteEntry.js"
 
       
     
