b3d207307dd4c9a460c9ac626d4da88c88c1f065
rjjackson22 <38430762+rjjackson22@users.noreply.github.com>
2026-05-25 19:23:00 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/1d1ea17f-3b2c-4887-8cb4-0df6c4461a78/exposeRemote.js
+++ src/Resources/public/studio/build/1d1ea17f-3b2c-4887-8cb4-0df6c4461a78/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/dcadbcc6-c4f7-4771-8e3c-6356a4a0f535/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/1d1ea17f-3b2c-4887-8cb4-0df6c4461a78/static/js/remoteEntry.js"
 
       
     
