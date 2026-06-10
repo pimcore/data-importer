647aecddf8345cd9b85c7dc8f5c380737a9228c7
markus-moser <markus.moser@pimcore.com>
2026-06-10 17:00:29 +0200

    [Studio] Fix classification store key picker (#631)

---
+++ src/Resources/public/studio/build/aa9d788c-db97-47bc-aa11-bad686a7e226/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/aa9d788c-db97-47bc-aa11-bad686a7e226/static/js/remoteEntry.js"
+
+      
+    
