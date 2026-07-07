e1039e4eb17852f3eb059f08e7e29dacc39a9bec
bluvulture <7668379+bluvulture@users.noreply.github.com>
2026-07-07 10:11:49 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/95f671da-9389-42d3-a967-b93ab1a220e8/exposeRemote.js
+++ src/Resources/public/studio/build/95f671da-9389-42d3-a967-b93ab1a220e8/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/6a407c0e-3bcc-4fa2-a16b-6d9c08284c49/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/95f671da-9389-42d3-a967-b93ab1a220e8/static/js/remoteEntry.js"
 
       
     
