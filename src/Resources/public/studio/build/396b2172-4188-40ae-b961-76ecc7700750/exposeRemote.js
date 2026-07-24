23fa05d01348ed8799547ae13bcfe664e6ff0579
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2026-07-24 09:54:03 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/396b2172-4188-40ae-b961-76ecc7700750/exposeRemote.js
+++ src/Resources/public/studio/build/396b2172-4188-40ae-b961-76ecc7700750/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/dceff20a-95c5-4d40-80c5-36971877f3b1/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/396b2172-4188-40ae-b961-76ecc7700750/static/js/remoteEntry.js"
 
       
     
