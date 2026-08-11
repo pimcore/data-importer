1223fa68aed75f124a9ccd501c501e95cd3cbfdc
lukmzig <30526586+lukmzig@users.noreply.github.com>
2026-08-11 13:52:24 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/6220738c-b9e2-4664-b6ac-574ed5a44766/exposeRemote.js
+++ src/Resources/public/studio/build/6220738c-b9e2-4664-b6ac-574ed5a44766/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/e5b235dd-aa7f-4291-bbc8-dd89d0e4f601/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/6220738c-b9e2-4664-b6ac-574ed5a44766/static/js/remoteEntry.js"
 
       
     
