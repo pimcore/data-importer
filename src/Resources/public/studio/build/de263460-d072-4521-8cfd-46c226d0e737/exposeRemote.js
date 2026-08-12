0df03a5001dcb30ddccb3b7dff80e7bcd819189b
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2026-08-12 07:22:58 +0200

    [Studio] - missing sorting option (#671)
    * add sorting options for logger tab
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Resources/public/studio/build/de263460-d072-4521-8cfd-46c226d0e737/exposeRemote.js
+++ src/Resources/public/studio/build/de263460-d072-4521-8cfd-46c226d0e737/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/e5b235dd-aa7f-4291-bbc8-dd89d0e4f601/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/de263460-d072-4521-8cfd-46c226d0e737/static/js/remoteEntry.js"
 
       
     
