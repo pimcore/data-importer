e28cfcad2821368b5e545515ade5fe8617d01bd9
xIrusux <42359615+xIrusux@users.noreply.github.com>
2026-08-13 10:29:53 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/9b2ad058-9c5b-4857-8065-6518c1ed5f55/exposeRemote.js
+++ src/Resources/public/studio/build/9b2ad058-9c5b-4857-8065-6518c1ed5f55/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/2a905a04-f4aa-48c3-9c0b-71bd2ecfce53/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/9b2ad058-9c5b-4857-8065-6518c1ed5f55/static/js/remoteEntry.js"
 
       
     
