59a9a82ca7917cb5a0bc6b2b503925360bbf4eef
fashxp <8792145+fashxp@users.noreply.github.com>
2026-08-19 18:06:23 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/0d00ac8e-703b-4c33-82bc-1b4590cfd87f/exposeRemote.js
+++ src/Resources/public/studio/build/0d00ac8e-703b-4c33-82bc-1b4590cfd87f/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/2e0c8b51-94f0-4373-8965-ed9a96d05e5c/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/0d00ac8e-703b-4c33-82bc-1b4590cfd87f/static/js/remoteEntry.js"
 
       
     
