beb561dbf6f17dc7857a9fb9af99d998cd7bcf89
idaiv <138690998+idaiv@users.noreply.github.com>
2026-08-28 12:23:23 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/69a8a440-1e7b-40d4-a7df-91f40b940018/exposeRemote.js
+++ src/Resources/public/studio/build/69a8a440-1e7b-40d4-a7df-91f40b940018/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/6481eecf-8266-4dbb-b075-b1c5d7c3e85b/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/69a8a440-1e7b-40d4-a7df-91f40b940018/static/js/remoteEntry.js"
 
       
     
