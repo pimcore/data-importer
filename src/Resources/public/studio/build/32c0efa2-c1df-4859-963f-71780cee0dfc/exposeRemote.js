3dc6ba7419eb805bb3060eeda1c1923a015b80cf
lukemacausland <58705994+lukemacausland@users.noreply.github.com>
2026-09-15 08:23:26 -0300

    Bug: Missing Time Field Type (#652)
    Add the time data type to the DEFAULT_TYPE transformation group so time fields on data objects can be targeted in importer mappings.
    
    Closes https://github.com/pimcore/platform-version/issues/276

---
+++ src/Resources/public/studio/build/32c0efa2-c1df-4859-963f-71780cee0dfc/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_dataimporter_bundle = "/bundles/pimcoredataimporter/studio/build/32c0efa2-c1df-4859-963f-71780cee0dfc/static/js/remoteEntry.js"
+
+      
+    
